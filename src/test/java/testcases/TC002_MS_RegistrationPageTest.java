6e3bc0897eecf2bceb99efc8649885d4f42799a8
unknown <shebasaravanan@gmail.com>
2022-05-04 00:31:07 -0700

    third commit

--- src/test/java/testcases/TC002_MS_RegistrationPageTest.java
+++ src/test/java/testcases/TC002_MS_RegistrationPageTest.java
@@ -3,12 +3,14 @@ package testcases;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+
+import com.relevantcodes.extentreports.LogStatus;
+
 import base.BaseClass;
 import pageclasses.LandingPage;
 import pageclasses.LoginPage;
 import pageclasses.MyAccountPage;
 import pageclasses.RegisterPage;
-import com.relevantcodes.extentreports.LogStatus;
 
 public class TC002_MS_RegistrationPageTest extends BaseClass {
 
@@ -27,12 +29,12 @@ public class TC002_MS_RegistrationPageTest extends BaseClass {
 
 //Clicking on Create an account will happen inside the method itself.
 		logpage = new LoginPage(driver);
-		logpage.setNewUserEmailAddress("reachautotest1269@gmail.com");
+		logpage.setNewUserEmailAddress("reachautotest1270@gmail.com");
 		test.log(LogStatus.INFO, "Clicked on Created a new account in Login Page");
 
 		regpage = new RegisterPage(driver);
-		regpage.validateRegister("Johny", "HartMan", "test1269", "Johny", "HartMan", "1269 HomeWood", "Moline", 
-				"13", "61275","21", "6475645022", "1269 SpringValley");
+		regpage.validateRegister("Jeff", "HartMan", "test1270", "Jeff", "HartMan", "1271 HomeWood", "Moline", 
+				"13", "61275","21", "6475645022", "1271 SpringValley");
 		Thread.sleep(1000);
 		test.log(LogStatus.INFO, "Registered a new customer in Registration Page");
 
@@ -44,7 +46,7 @@ public class TC002_MS_RegistrationPageTest extends BaseClass {
 		asrt.assertEquals(act_Title, exp_Title);
 		test.log(LogStatus.INFO, "Asserted page Title Successfully after Registration");
 		
-		String exp_name = "Johny HartMan";
+		String exp_name = "Jeff HartMan";
 		String act_name = acctpage.validateRegisteredCustomerName();
 		System.out.println("Registered Customer name: " + act_name);
 		asrt.assertEquals(act_name, exp_name);
