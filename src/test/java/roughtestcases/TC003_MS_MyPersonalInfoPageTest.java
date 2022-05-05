6dca2f059f61fcf784898430dde05cf8153761e2
unknown <shebasaravanan@gmail.com>
2022-05-04 18:28:54 -0700

    sixth commit

--- src/test/java/roughtestcases/TC003_MS_MyPersonalInfoPageTest.java
+++ src/test/java/roughtestcases/TC003_MS_MyPersonalInfoPageTest.java
@@ -26,7 +26,7 @@ public class TC003_MS_MyPersonalInfoPageTest extends BaseClass {
 
 //Here updating password and clicking on save
 		perinfopage = new MypersonalInformationPage(driver);
-		perinfopage.validateChangePassword("test1302", "test1303", "test1303");
+		perinfopage.validateChangePassword("test1306", "test1307", "test1307");
 		test.log(LogStatus.INFO, "Changed the password with new Passwords in MyPersonalInfo Page");
 //Assertions
 		idpage = new IdentityPage(driver);
@@ -50,7 +50,7 @@ public class TC003_MS_MyPersonalInfoPageTest extends BaseClass {
 	{
 //Relogin again with changed Password and Assertion of page Title
 		logpage = new LoginPage(driver);
-		logpage.doLogin("reachautotest1302@gmail.com", "test1303");
+		logpage.doLogin("reachautotest1306@gmail.com", "test1307");
 		acctpage = new MyAccountPage(driver);
 		String act_title = acctpage.validatePageTitle();
 		String exp_title = "My account - My Store";
