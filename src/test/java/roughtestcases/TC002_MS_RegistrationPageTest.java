058d08eebf886f85e4f331efd38598540350039c
unknown <shebasaravanan@gmail.com>
2022-05-04 20:03:58 -0700

    seventh commit

--- src/test/java/roughtestcases/TC002_MS_RegistrationPageTest.java
+++ src/test/java/roughtestcases/TC002_MS_RegistrationPageTest.java
@@ -29,12 +29,12 @@ public class TC002_MS_RegistrationPageTest extends BaseClass {
 
 //Clicking on Create an account will happen inside the method itself.
 		logpage = new LoginPage(driver);
-		logpage.setNewUserEmailAddress("reachautotest1306@gmail.com");
+		logpage.setNewUserEmailAddress("reachautotest1400@gmail.com");
 		test.log(LogStatus.INFO, "Clicked on Created a new account in Login Page");
 
 		regpage = new RegisterPage(driver);
-		regpage.validateRegister("James", "Oliver", "test1306", "James", "Oliver", "1306 HomeWood", "Moline", 
-				"13", "61275","21", "6475645022", "1306 SpringValley");
+		regpage.validateRegister("Jame", "Oliver", "test1400", "Jame", "Oliver", "1400 HomeWood", "Moline", 
+				"13", "61275","21", "6475645022", "1400 SpringValley");
 		Thread.sleep(1000);
 		test.log(LogStatus.INFO, "Registered a new customer in Registration Page");
 
@@ -46,7 +46,7 @@ public class TC002_MS_RegistrationPageTest extends BaseClass {
 		asrt.assertEquals(act_Title, exp_Title);
 		test.log(LogStatus.INFO, "Asserted page Title Successfully after Registration");
 		
-		String exp_name = "James Oliver";
+		String exp_name = "Jame Oliver";
 		String act_name = acctpage.validateRegisteredCustomerName();
 		System.out.println("Registered Customer name: " + act_name);
 		asrt.assertEquals(act_name, exp_name);
--- src/test/java/roughtestcases/TC003_MS_MyPersonalInfoPageTest.java
+++ src/test/java/roughtestcases/TC003_MS_MyPersonalInfoPageTest.java
@@ -26,7 +26,7 @@ public class TC003_MS_MyPersonalInfoPageTest extends BaseClass {
 
 //Here updating password and clicking on save
 		perinfopage = new MypersonalInformationPage(driver);
-		perinfopage.validateChangePassword("test1306", "test1307", "test1307");
+		perinfopage.validateChangePassword("test1400", "test1401", "test1401");
 		test.log(LogStatus.INFO, "Changed the password with new Passwords in MyPersonalInfo Page");
 //Assertions
 		idpage = new IdentityPage(driver);
@@ -50,7 +50,7 @@ public class TC003_MS_MyPersonalInfoPageTest extends BaseClass {
 	{
 //Relogin again with changed Password and Assertion of page Title
 		logpage = new LoginPage(driver);
-		logpage.doLogin("reachautotest1306@gmail.com", "test1307");
+		logpage.doLogin("reachautotest1400@gmail.com", "test1401");
 		acctpage = new MyAccountPage(driver);
 		String act_title = acctpage.validatePageTitle();
 		String exp_title = "My account - My Store";
